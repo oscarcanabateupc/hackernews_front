a521ed6035f4d0c4bb49c36453fa065fadc5e268
Tu Nombre <you@example.com>
2021-12-11 19:19:02 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewsEntryComponent } from './news-entry/news-entry.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hackernews_front';
+  a = new NewsEntryComponent();
+  b = new NewsEntryComponent();
+  news_entries : NewsEntryComponent[] = [this.a,this.b,this.b,this.b];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsEntryComponent } from './news-entry/news-entry.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsEntryComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
